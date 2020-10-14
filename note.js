a301876b2b49f2f0eab0797b91b60db6764ace4c
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-10-14 19:56:46 -0300

    finished lesson, starting challenge 2

--- note.js
+++ note.js
@@ -1,13 +1,7 @@
-const fs = require('fs');
+const utils = require('./utils');
 
-// fs.writeFileSync(1'notes.txt', 'My name is Mud.\n');
-
-
-//
-// Challenge: Append a message to notes.txt
-//
-// 1. Use appendFileSync to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
-
-fs.appendFileSync('notes.txt', 'I live in Mud Town Pennsilvania.\n');
+console.log(
+  utils.name,
+  utils.age,
+  utils.add(1, 3)
+);
