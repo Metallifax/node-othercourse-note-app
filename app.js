af0078839c298e0341e07c896ca0720d86e76272
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-10-14 21:52:31 -0300

    built the skeleton for the arg-parse program, starting database

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
 // npm modules
 const chalk = require('chalk');
-
+const yargs = require('yargs');
 // User defined modules
 const read = require('./notes');
 
-// Arguments
-if (command === 'add') console.log('Adding note!');
-else if (command === 'remove') console.log('Removing!');
-else console.log('Did not compute...');
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Body content',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv){
+    console.log('Title: ' + argv.title +'\nBody: ' + argv.body);
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function(){
+    console.log('Removing the note...');
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'read a note',
+  handler: function(){
+    console.log('Read a note...');
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'Show all notes',
+  handler: function(){
+    console.log('Here\'s your notes!');
+  }
+});
+
+yargs.parse();
