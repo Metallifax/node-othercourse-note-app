9becddf474722dd58cfdafe32f7a59f686444fe7
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-10-14 21:03:20 -0300

    start the actual app creation

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const validator = require('validator');
 const read = require('./notes');
 const chalk = require('chalk');
 
-// console.log(validator.isURL('google.com'));
 getNotes = () => console.log(`Your notes...\n${read()}`);
 getSuccess = () => console.log(chalk.red('Su') + chalk.green('cce') + chalk.blue('ss!'));
 
